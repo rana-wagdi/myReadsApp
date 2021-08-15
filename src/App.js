0dd98099a2ab13ac700f5470692b51ada7d28f4e
Rana Wagdi <73128713+rana2020701@users.noreply.github.com>
2021-08-15 07:46:56 -0700

    add nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import Header from './components/Header/Header';
 
 import './App.scss'
 function App() {
   return (
-   <div>
-     
-   </div>
+   <Header />
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+
+
+
+const Header = props => {
+    return <nav className="header">
+            <h1>MyReads</h1>
+            <ul>
+                <li>Read</li>
+                <li>Currently Read</li>
+                <li>Want To Read</li>
+            </ul>
+    </nav>
+}
+
+
+export default Header;
